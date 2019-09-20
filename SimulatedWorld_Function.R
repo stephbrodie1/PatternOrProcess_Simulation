57b831ac0fbdbe225fd3efc20d206012f7905e6f
stephbrodie1 <stephbrodie1@gmail.com>
2019-09-19 18:23:10 -0700

    Updates from review

--- SimulatedWorld_Function.R
+++ SimulatedWorld_Function.R
@@ -1,6 +1,7 @@
 #Function to simulate species distribution and abundance with respect to spatiotemporal and environmental covariates
-#Set working directory to the repository 
+# Brodie et al.(2019).Ecography. Trade-offs in covariate selection for species distribution models: a methodological comparison. doi: 10.1111/ecog.04707
 
+#Set working directory to the repository 
 setwd('~/PatterOrProcess_Simulation/')
 
 SimulateWorld <- function(){
@@ -35,7 +36,7 @@ SimulateWorld <- function(){
   vals <- seq(1,20)
   lon <-raster::calc(lon, fun = function(x) jitter(x,amount=1))
   lon[] <- rescale(lon@data@values, to = c(1,20))
-  if (cor(lon@data@values,depth@data@values)>0.6) stop('lon~depth correlation >0.6') 
+  if (cor(lon@data@values,depth@data@values)>0.6) stop('lon~depth correlation is too high. Re-run.') 
   
   #Generate Latitude Covariate
   lat <- raster(ncol=20,nrow=20)
@@ -62,18 +63,18 @@ SimulateWorld <- function(){
   for (y in years){
     print(paste0("Creating environmental simulation for Year ",y))
     
-    #Generate SeaIce Covariate
-    high_years <- c(1,2,3,8,9,10,11) #Decide which years for each sea ice mode
+    #Generate Spatiotemporal Covariate
+    high_years <- c(1,2,3,8,9,10,11) #Decide which years for each mode
     medium_years <- c(4,5,6,7,12,13,18,19,20)
     low_years <- c(14,15,16,17)
     if (y %in% high_years){
-      spatiotemp <- raster("SeaIceShape_High.png")
+      spatiotemp <- raster("Shape_High.png")
     }
     if (y %in% medium_years){
-      spatiotemp <- raster("SeaIceShape_Medium.png")
+      spatiotemp <- raster("Shape_Medium.png")
     }
     if (y %in% low_years){
-      spatiotemp <- raster("SeaIceShape_Low.png")
+      spatiotemp <- raster("Shape_Low.png")
     }
     ex <- extent(0.5,20.5,0.5,20.5)
     extent(spatiotemp) <- ex
@@ -103,10 +104,14 @@ SimulateWorld <- function(){
     # plot(envirosuitability$suitab.raster) #plot habitat suitability
     # virtualspecies::plotResponse(envirosuitability) #plot response curves
     
-    #----Combing all habitat suitability rasters----
+    #----Combining all habitat suitability rasters----
     suitability_combo <- ((envirosuitability$suitab.raster*2)  + spatiotemp*1 + (latlon_suitability$suitab.raster*1)) / 4 
     suitability_combo[] <- rescale(suitability_combo@data@values, to=c(0,1))
     # plot(suitability_combo) #Plot final habitat suitability layer
+    
+    #----Convert suitability to Presence-Absence----
+    suitability_PA <- convertToPA(suitability_combo, PA.method = "probability", beta = 0.5,
+                                  alpha = -0.05, species.prevalence = NULL, plot = FALSE)
     
     #----Extract suitability for each survey location----
     print("Extracting suitability")
@@ -114,23 +119,28 @@ SimulateWorld <- function(){
       start_index <- min(which(grid$year==y))
       ii = (i + start_index) -1
       s <- raster::extract(suitability_combo,grid[ii,1:2]) 
+      pa <- raster::extract(suitability_PA$pa.raster,grid[ii,1:2])
       t <- raster::extract(temp,grid[ii,1:2]) 
       d <- raster::extract(depth,grid[ii,1:2])
       h <- raster::extract(spatiotemp,grid[ii,1:2])
       grid$suitability[ii] <-  s
+      grid$pres[ii] <-  pa
       grid$temp[ii] <-  t
       grid$depth[ii] <-  d
-      grid$seaice[ii] <-  h
+      grid$spatiotemp[ii] <-  h
     }
   }
   summary(grid)
   
-  #----Generate presence and abundance as a function of habitat suitability----
-  grid$pres <- ifelse(grid$suitability<=0.4,0, ifelse(grid$suitability>=0.6,1,rbinom(nrow(grid),1,prob=grid$suitability)))
-  grid$catch <- ifelse(grid$pres==1,rlnorm(1,2,0.1)*grid$suitability,0)
+  #----Create abundance as a function of the environment----
+  grid$abundance <- ifelse(grid$pres==1,rlnorm(1,2,0.1)*grid$suitability,0)
   
   return(grid)
 }
 
 
+#----EXAMPLE----
+# data <- SimulateWorld()
+# #plot time-series of total catch
+# plot(aggregate(abundance~year,data,FUN="sum"),type="l",ylab="Abundance")
 
